989217f1fbb60afa9bd6bba1fcf915e0fc49cf57
UnicornDad8 <unicorndad8@gmail.com>
2024-10-12 20:07:01 -0300

    Add AnimatedCounter, DoughnutChart, and TotalBalanceBox components, also some dependencies to build these components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,5 @@
 import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
 
 const Home = () => {
   const loggedIn = { firstName: "Adrian" };
@@ -12,6 +13,11 @@ const Home = () => {
             title="Welcome"
             user={loggedIn?.firstName || "Guest"}
             subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
           />
         </header>
       </div>
