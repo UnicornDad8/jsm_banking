7c8ba9f1a1874c934946e0810a8b70c96b9bcd30
UnicornDad8 <unicorndad8@gmail.com>
2024-10-16 16:00:31 -0300

    Add BankCard component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -27,7 +27,11 @@ const Home = () => {
         </header>
         RECENT TRANSACTIONS
       </div>
-      <RightSidebar user={loggedIn} transactions={[]} banks={[]} />
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, { currentBalance: 500.5 }]}
+      />
     </section>
   );
 };
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
+import BankCard from "./BankCard";
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
   return (
@@ -30,6 +31,29 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
             <h2 className="text-14 font-semibold">Add Bank</h2>
           </Link>
         </div>
+
+        {banks?.length > 0 && (
+          <div className="relative flex flex-1 flex-col items-center justify-center gap-5">
+            <div className="relative z-10">
+              <BankCard
+                key={banks[0].$id}
+                account={banks[0]}
+                userName={`${user.firstName} ${user.lastName}`}
+                showBalance={false}
+              />
+            </div>
+            {banks[1] && (
+              <div className="absolute right-0 top-8 z-0 w-[90%]">
+                <BankCard
+                  key={banks[1].$id}
+                  account={banks[1]}
+                  userName={`${user.firstName} ${user.lastName}`}
+                  showBalance={false}
+                />
+              </div>
+            )}
+          </div>
+        )}
       </section>
     </aside>
   );
